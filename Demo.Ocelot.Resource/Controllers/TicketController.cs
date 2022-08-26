0452bed39c66e91b62791919cbbc3d13a7257cd9
Emad Gelani <a.egelani@dev.thiqah.sa>
2022-08-26 20:30:20 +0300

    Add project files.

---
+++ Demo.Ocelot.Resource/Controllers/TicketController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Ocelot.Resource.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<Ticket> GetTicket()
+        {
+            var ticket = new Ticket { Id = 1, Description = "asd" };
+            return Ok(ticket);
+        }
+        [HttpPost]
+        public ActionResult<Ticket> AddTicket([FromBody] Ticket ticket)
+        {
+            return Ok(ticket);
+        }
+    }
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
